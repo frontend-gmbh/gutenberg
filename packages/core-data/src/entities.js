7acc692963840bef049d1c5b8ee64c4df37dc06a
Chris Zarate <chris.zarate@automattic.com>
2025-11-06 17:13:50 -0700

    Real-time collaboration: [no-pr] Sync collections
    * Real-time collaboration: Sync collections
    
    * Create markEntityAsSaved utility function
    
    * Improve comment

--- packages/core-data/src/entities.js
+++ packages/core-data/src/entities.js
@@ -16,8 +16,7 @@ import { __ } from '@wordpress/i18n';
 import { getSyncManager } from './sync';
 import {
 	applyPostChangesToCRDTDoc,
-	defaultApplyChangesToCRDTDoc,
-	defaultGetChangesFromCRDTDoc,
+	defaultSyncConfig,
 	getPostChangesFromCRDTDoc,
 } from './utils/crdt';
 
@@ -220,7 +219,20 @@ export const rootEntitiesConfig = [
 		plural: 'fontCollections',
 		key: 'slug',
 	},
-];
+].map( ( entity ) => {
+	const syncEnabledRootEntities = new Set( [ 'comment' ] );
+
+	// Add using `if` statement to preserve tree-shaking based on IS_GUTENBERG_PLUGIN.
+	if (
+		syncEnabledRootEntities.has( entity.name ) &&
+		window.__experimentalEnableSync
+	) {
+		if ( globalThis.IS_GUTENBERG_PLUGIN ) {
+			entity.syncConfig = defaultSyncConfig;
+		}
+	}
+	return entity;
+} );
 
 export const deprecatedEntities = {
 	root: {
@@ -403,7 +415,7 @@ async function loadTaxonomyEntities() {
 	} );
 	return Object.entries( taxonomies ?? {} ).map( ( [ name, taxonomy ] ) => {
 		const namespace = taxonomy?.rest_namespace ?? 'wp/v2';
-		return {
+		const entity = {
 			kind: 'taxonomy',
 			baseURL: `/${ namespace }/${ taxonomy.rest_base }`,
 			baseURLParams: { context: 'edit' },
@@ -412,6 +424,15 @@ async function loadTaxonomyEntities() {
 			getTitle: ( record ) => record?.name,
 			supportsPagination: true,
 		};
+
+		// Add using `if` statement to preserve tree-shaking based on IS_GUTENBERG_PLUGIN.
+		if ( window.__experimentalEnableSync ) {
+			if ( globalThis.IS_GUTENBERG_PLUGIN ) {
+				entity.syncConfig = defaultSyncConfig;
+			}
+		}
+
+		return entity;
 	} );
 }
 
@@ -430,15 +451,10 @@ async function loadSiteEntity() {
 		meta: {},
 	};
 
+	// Add using `if` statement to preserve tree-shaking based on IS_GUTENBERG_PLUGIN.
 	if ( window.__experimentalEnableSync ) {
 		if ( globalThis.IS_GUTENBERG_PLUGIN ) {
-			/**
-			 * @type {import('@wordpress/sync').SyncConfig}
-			 */
-			entity.syncConfig = {
-				applyChangesToCRDTDoc: defaultApplyChangesToCRDTDoc,
-				getChangesFromCRDTDoc: defaultGetChangesFromCRDTDoc,
-			};
+			entity.syncConfig = defaultSyncConfig;
 		}
 	}
 
