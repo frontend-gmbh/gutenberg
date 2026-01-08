e633d76cb8348edf514914280f2c69e3de453e9a
Jerry Jones <jones.jeremydavid@gmail.com>
2026-01-08 11:53:21 -0600

    Add useEffect to force navigations within overlays to never have an overlay

--- packages/block-library/src/navigation/edit/index.js
+++ packages/block-library/src/navigation/edit/index.js
@@ -488,12 +488,15 @@ function Navigation( {
 	);
 	const isWithinOverlayTemplatePart = templatePart?.area === 'overlay';
 
-	// Force overlayMenu to 'never' if within an overlay template part.
-	const effectiveOverlayMenu = isWithinOverlayTemplatePart
-		? 'never'
-		: overlayMenu;
+	// Force overlayMenu to 'never' if within an overlay template part
+	// to prevents overlays within overlays.
+	useEffect( () => {
+		if ( isWithinOverlayTemplatePart && overlayMenu !== 'never' ) {
+			setAttributes( { overlayMenu: 'never' } );
+		}
+	}, [ isWithinOverlayTemplatePart, overlayMenu, setAttributes ] );
 
-	const isResponsive = 'never' !== effectiveOverlayMenu;
+	const isResponsive = 'never' !== overlayMenu;
 	const blockProps = useBlockProps( {
 		ref: navRef,
 		className: clsx(
@@ -845,7 +848,7 @@ function Navigation( {
 	);
 
 	const accessibleDescriptionId = `${ clientId }-desc`;
-	const isHiddenByDefault = 'always' === effectiveOverlayMenu;
+	const isHiddenByDefault = 'always' === overlayMenu;
 	const isManageMenusButtonDisabled =
 		! hasManagePermissions || ! hasResolvedNavigationMenus;
 
