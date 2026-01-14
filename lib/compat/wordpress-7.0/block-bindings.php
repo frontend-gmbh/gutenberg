fb14e141ed0ebc7db0ed65116ffd6cb1c4656386
Bernie Reiter <ockham@raz.or.at>
2026-01-14 15:13:35 +0100

    Replace img src attribute in core/cover block.

--- lib/compat/wordpress-7.0/block-bindings.php
+++ lib/compat/wordpress-7.0/block-bindings.php
@@ -20,3 +20,25 @@ add_filter(
 	10,
 	2
 );
+
+/**
+ * Callback function for the render_block filter.
+ *
+ * @since 7.0.0
+ *
+ * @param string   $block_content The block content.
+ * @param array    $block         The full block, including name and attributes.
+ */
+function gutenberg_block_bindings_replace_cover_block_img_src( $block_content, $block, $instance ) {
+	$amended_content = new WP_HTML_Tag_Processor( $block_content );
+	if ( ! $amended_content->next_tag(
+		array(
+			'tag_name' => 'img'
+		)
+	) ) {
+		return $block_content;
+	}
+	$amended_content->set_attribute( 'src', $instance->attributes['url'] );
+	return $amended_content->get_updated_html();
+}
+add_filter( 'render_block_core/cover', 'gutenberg_block_bindings_replace_cover_block_img_src', 10, 3 );
