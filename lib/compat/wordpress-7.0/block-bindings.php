f7b57514983f96c54c8d2f28f9b895dfaa686244
Bernie Reiter <ockham@raz.or.at>
2026-01-14 13:17:01 +0100

    Block Bindings: Add support for Cover block id and url attrs

---
+++ lib/compat/wordpress-7.0/block-bindings.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Block Bindings: Support for generically setting rich-text block attributes.
+ *
+ * @since 7.0
+ * @package gutenberg
+ * @subpackage Block Bindings
+ */
+
+// The following filter can be removed once the minimum required WordPress version is 7.0 or newer.
+add_filter(
+	'block_bindings_supported_attributes',
+	function ( $attributes, $block_type ) {
+		if ( 'core/cover' === $block_type ) {
+			$attributes[] = 'id';
+			$attributes[] = 'url';
+		}
+		return $attributes;
+	},
+	10,
+	2
+);
